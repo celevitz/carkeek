e39e447910edebe3e2cfba906ce2a709c9bb35c6
Carly <celevitz@gmail.com>
2023-06-24 14:34:26 -0700

    Move data cleaning to separate code

--- WatershedData.R
+++ WatershedData.R
@@ -47,53 +47,7 @@ logo <- paste(directory,"CWCAPlogo-1-white-1024x346.png",sep="")
 
 ## Step 2: Bring in data and clean it
 ### Step 2a. Bring in data
-rawdata <- as_tibble(read.xlsx(paste(directory,"H20 Data as of May 2024.xlsx"
-                                     ,sep=""),sheet=1,startRow = 2))
-
-### Step 2b. Clean data
-clean <- rawdata %>%
-  # Fix date error: 12/13/2020 should be 12/13/2022
-  mutate(Date.Tested = ifelse(Date.Tested == 44178 & !(is.na(Date.Tested))
-                              ,44908,Date.Tested)
-  # Fix the date and time
-        ,Date.Tested = as.Date(as.numeric(Date.Tested), origin = "1899-12-30")
-  # remove trailing spaces, extra quotation marks
-        ,`Tester.#1` = trimws(gsub("\\\"","",`Tester.#1`),"both")
-        ,`Tester.#2` = trimws(gsub("\\\"","",`Tester.#2`),"both")
-  # combine testers that are the same
-        ,`Tester.#1` = case_when(`Tester.#1` %in% c("Sue") ~ "Sue Cottrell"
-                                 ,`Tester.#1` %in% c("Natale") ~ "Natalie"
-                                 ,`Tester.#1` %in% c("mike") ~ "Mike"
-                                 ,`Tester.#1` %in% c("sean") ~ "Sean"
-                                 ,TRUE ~ `Tester.#1`)
-         ,`Tester.#2` = case_when(`Tester.#2` %in% c("Alice") ~
-                                                        "Alice Cottrell-Steen"
-                                  ,`Tester.#2` %in% c("-  ","-") ~ NA
-                                  ,TRUE ~ `Tester.#2`)
-  # If Turbidity is greater than 240, plot it at 241
-        ,`Turbidity.(JTU)` = ifelse(">240 NCU","241",`Turbidity.(JTU)`)
-  # If Coliform is too numerous to count (TNTC), plot it at 350
-        ,Coliform.1 = case_when(Coliform.1 %in% c("TNTC","TNTC ")~"350"
-                                ,Coliform.1 %in% c("-","n/a") ~NA
-                                ,TRUE ~ Coliform.1)
-        ,Coliform.2 = case_when(Coliform.2 %in% c("TNTC","TNTC ")~"350"
-                                ,Coliform.2 %in% c("-","n/a") ~NA
-                                ,TRUE ~ Coliform.2)
-        ,Coliform.3 = case_when(Coliform.3 %in% c("TNTC","TNTC ")~"350"
-                                ,Coliform.3 %in% c("-","n/a") ~NA
-                                ,TRUE ~ Coliform.3)
-  ) %>%
-  # drop the fake data
-  filter(`Tester.#1` != "John Doe")
-
-  # change characters variables that should be numeric, to numeric
-  for (charvar in c("Average.DO","%.Ox..Sat.","Total.ALK","Total.Hardness"
-                    ,"Turbidity.(JTU)","E..coli.3","Avg..E..coli"
-                    ,"Coliform.1","Coliform.2","Coliform.3","Avg..Coliform")) {
-    print(charvar)
-    clean[,charvar] <- as.numeric(unlist(clean[,charvar]))
-  }
-
+clean <- read.csv(paste0(directory,"H20Data.csv"),stringsAsFactors = FALSE)
 
 ## Step 3: set up things for visualizations
 
