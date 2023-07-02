2ec440c748c1536719af1fe038a74c8c6e72241c
Carly <celevitz@gmail.com>
2023-07-02 10:47:51 -0700

    Simplify and use ggplot instead of ggarrange a bunch of separate plots into a line

--- QuarterlyReport.R
+++ QuarterlyReport.R
@@ -113,76 +113,62 @@ previousq <- clean %>%
     # Number of times each of them volunteered
     pqAveragetimesvolunteered <- pqNumberoftests/pqNumVols
 
-## Step 5. Change between quarters
-    # Number of tests
-      if (qNumberoftests < pqNumberoftests) {
-        chNumberoftests <- paste0(pqNumberoftests-qNumberoftests
-                                  ," fewer tests than")
-      } else if (qNumberoftests > pqNumberoftests) {
-        chNumberoftests <- paste0(qNumberoftests-pqNumberoftests
-                                  ," more tests than")
-      } else {
-        chNumberoftests <- " the same number of tests as"
-      }
 
-    # Number of unique volunteers
-      if (qNumVols < pqNumVols) {
-        chNumberofVols<- paste0(pqNumVols-qNumVols
-                                  ," fewer volunteers than")
-      } else if (qNumVols > pqNumVols) {
-        chNumberofVols <- paste0(qNumVols-pqNumVols
-                                  ," more volunteers than")
-      } else {
-        chNumberofVols <- " the same number of volunteers as"
-      }
-
-    # Number of volunteer hours
-      if (qVolTime < pqVolTime) {
-        chVolHrs <- paste0(pqVolTime-qVolTime
-                                ," fewer hours than")
-      } else if (qVolTime > pqVolTime) {
-        chVolHrs <- paste0(qVolTime-pqVolTime
-                                 ," more hours than")
-      } else {
-        chVolHrs <- " the same number of hours as"
-      }
-
-    # Avg number of times volunteered
-      if (qAveragetimesvolunteered < pqAveragetimesvolunteered) {
-        chAvgTimes <- paste0(pqAveragetimesvolunteered-qAveragetimesvolunteered
-                           ," fewer times on average")
-      } else if (qAveragetimesvolunteered > pqAveragetimesvolunteered) {
-        chAvgTimes <- paste0(qAveragetimesvolunteered-pqAveragetimesvolunteered
-                           ," more times on average")
-      } else {
-        chAvgTimes <- " the same number of times on average"
-      }
-
-## Step 6. year to date
+## Step 5. year to date
 
 
 
 
+## Step 6. Create dataframe of this information
+  db <- as.data.frame(matrix(c(c("Tests",qNumberoftests,pqNumberoftests
+                                 ,NA,5)
+                         ,c("% of tests done in pairs",round(
+                            qNumberoftestsinpairs/qNumberoftests*100,1)
+                            ,round(pqNumberoftestsinpairs/pqNumberoftests*100,1)
+                            ,NA,4)
+                         ,c("Unique volunteers",qNumVols,pqNumVols
+                            ,NA,3)
+                         ,c("Hours",qVolTime,pqVolTime,NA,2)
+                         ,c("Average times volunteered"
+                            ,qAveragetimesvolunteered,pqAveragetimesvolunteered
+                            ,NA,1))
+                       ,ncol=5,byrow = TRUE
+                       ,dimnames=list(NULL
+                                  ,c("measure","current","previous","ytd"
+                                     ,"y"))))
 
-### Step 7. Writing
-intro <- str_glue("These data are for {quarterNameLong}. The eight sites were tested
-                  a total of {qNumberoftests} times by {qNumVols} unique
-                  volunteers (an average of {qAveragetimesvolunteered} times
-                  volunteered). Because many of the tests (
-                  {qNumberoftestsinpairs} out of {qNumberoftests}) were done by
-                  two people, this was about {qVolTime} hours of volunteer
-                  effort. Volunteers completed {chNumberoftests} (and
-                  volunteered {chVolHrs}) last quarter, which was
-                  {pqNumberoftests} tests and an average of
-                  {pqAveragetimesvolunteered} times volunteered. There were
-                  {chNumberofVols} last quarter.")
+  db <- db %>%
+    mutate(current = as.numeric(current)
+           ,previous = as.numeric(previous)
+          ,quarterlyChange = current-previous
+          ,directionofchange = case_when(quarterlyChange < 0 ~ "negative"
+                                         ,quarterlyChange == 0 ~ "no change"
+                                         ,quarterlyChange > 0 ~ "positive"))
 
-# clean %>%
-#   ggplot(aes(x=dateTested,y=averageDo)) +
-#   theme_nothing() +
-#   labs(title=str_wrap(intro,90)) +
-#   theme(plot.title = element_text(family=ft)
-#         ,plot.margin = margin(10,10,10,30))
+### Step 7. Graph
+  dashboard <-
+  db %>%
+    ggplot(aes(x=0,y=y)) +
+    geom_text(aes(x=0,y=y,label = measure),hjust=0,family = ft) +
+    geom_text(aes(x=10,y=y,label = current),hjust=0,family = ft) +
+    geom_text(aes(x=20,y=y,label = previous),hjust=0,family = ft) +
+    geom_text(aes(x=31,y=y,label = quarterlyChange),hjust=0,family = ft) +
+    geom_point(aes(x=30,y=y,shape = directionofchange,color=directionofchange
+                   ,fill = directionofchange)) +
+    scale_shape_manual(values = c(25,20,24)) +
+    scale_color_manual(values=c(mid,mid2,main)) +
+    scale_fill_manual(values=c(mid,mid2,main)) +
+    scale_x_continuous(lim=c(0,60),breaks=seq(0,40,10)
+                       ,labels=c("Measure","Current quarter","Previous quarter"
+                                ,"Change since last quarter","Year-to-date")
+                       ,position = "top")+
+    theme_minimal()+
+    theme(panel.grid = element_blank()
+          ,axis.title = element_blank()
+          ,axis.text.y = element_blank()
+          ,axis.text.x = element_text(family = ft)
+          ,legend.position = "none" )
+
 
 ### Step 8. Quarterly dashboard
 ## Step 8a. Header
@@ -205,130 +191,14 @@ header2 <- clean %>%
         ,plot.title = element_text(color="white",family=ft,hjust=.5,face="bold"
                                    ,size = 32,margin=margin(30,0,0,0)))
 
-## Table Column Names
-tablecolumnnamesA <- quarter %>%
-  ggplot(aes(x=1,y=1)) +
-  geom_text(aes(x=0,y=1,family=ft),color=dark,size=textsize
-            ,label=paste0("\n\n\n",quarterNameMid," values") ) +
-  theme_nothing() +
-  geom_hline(aes(yintercept=0),color=dark)
-
-tablecolumnnamesB <- quarter %>%
-  ggplot(aes(x=1,y=1)) +
-  geom_text(aes(x=1,y=1,family=ft),color=dark,size=textsize
-            ,label=paste0("\n\nCompared to\n",previousquarterMid) ) +
-  theme_nothing() +
-  geom_hline(aes(yintercept=0),color=dark)
-
-tablecolumnnames <- ggarrange(tablecolumnnamesA,tablecolumnnamesB,
-                              nrow=1,ncol=2,widths=c(2,1))
-
-## Step 8b. function for each measure
-currentValue <- str_glue("{qNumberoftests}")
-previousValue <- str_glue("{pqNumberoftests}")
-mainlabel <- str_glue("tests")
-positivechangelabel <- paste0("+",qNumberoftests-pqNumberoftests)
-negativechangelabel <- paste0("-",pqNumberoftests-qNumberoftests)
-
-  graphFunction <- function(currentValue,previousValue,mainlabel
-                            ,positivechangelabel
-                            ,negativechangelabel) {
-
-    graphA <- quarter %>%
-      ggplot(aes(x=1,y=1)) +
-      geom_text(aes(x=1,y=1,family=ft),color=dark,size=titlesize
-                ,label=currentValue ) +
-      theme_nothing()
-
-    graphB <- quarter %>%
-      ggplot(aes(x=1,y=1)) +
-      geom_text(aes(x=1,y=1,family=ft),color=dark,size=subtitlesize
-                ,label=mainlabel) +
-      theme_nothing()
-
-    graphC <- previousq %>%
-      ggplot(aes(x=1,y=1)) +
-      theme_nothing() +
-      if (currentValue < previousValue) {
-          geom_point(aes(x=1,y=1),pch=25,color=mid,fill=mid,size=10)
-      } else if (currentValue > previousValue) {
-          geom_point(aes(x=1,y=1),pch=24,color=main,fill=main,size=10)
-      } else {
-          geom_point(aes(x=1,y=1),pch=20,color=mid2,fill=mid2,size=10)
-      }
-
-    graphD <- previousq %>%
-      ggplot(aes(x=1,y=1)) +
-      theme_nothing() +
-      if (currentValue < previousValue) {
-        geom_text(aes(x=1,y=1,family=ft),color=mid,size=textsize
-                  ,label=negativechangelabel)
-      } else if (currentValue > previousValue) {
-        geom_text(aes(x=1,y=1,family=ft),color=main,size=textsize
-                  ,label=positivechangelabel)
-      } else {
-        geom_text(aes(x=1,y=1,family=ft),color=mid2,size=textsize
-                  ,label="+0")
-      }
-
-    ggarrange(graphA,graphB
-              ,ggarrange(graphC,graphD,nrow=1,ncol=2)
-              ,nrow=1,ncol=3)
-
-  }
-
-## Step 8c. Times tested
-  timestested <- graphFunction(str_glue("{qNumberoftests}")
-                               ,str_glue("{pqNumberoftests}")
-                               ,str_glue("tests")
-                            ,paste0("+",qNumberoftests-pqNumberoftests
-                                    ," since last quarter")
-                            ,paste0("-",pqNumberoftests-qNumberoftests
-                                    ," since last quarter"))
-
-## Step 8d. Number of tests with two people
-  testswith2graph <- graphFunction(
-    str_glue("{round(qNumberoftestsinpairs/qNumberoftests*100 ,1)}%")
-    ,str_glue("{round(pqNumberoftestsinpairs/pqNumberoftests*100,1)}%")
-    ,str_glue("done with\na partner")
-    ,paste0("+",round((qNumberoftestsinpairs/qNumberoftests-
-                         pqNumberoftestsinpairs/pqNumberoftests)*100,1),"%"
-            ," since last quarter")
-    ,paste0("-",round((pqNumberoftestsinpairs/pqNumberoftests-
-                         qNumberoftestsinpairs/qNumberoftests)*100,1),"%"
-            ," since last quarter")
-  )
-
-## Step 8e. Number of volunteers
-  numvolsgraph <- graphFunction(
-    str_glue("{qNumVols}")
-    ,str_glue("{pqNumVols}")
-    ,str_glue("unique\nvolunteers")
-    ,paste0("+",qNumVols-pqNumVols," since last quarter")
-    ,paste0("-",pqNumVols-qNumVols," since last quarter")
-  )
-
-## Step 8f. Number of hours by volunteers this quarters
-  numHrsGraph <- graphFunction(
-    str_glue("{qVolTime}")
-    ,str_glue("{pqVolTime}")
-    ,str_glue("hours")
-    ,paste0("+",qVolTime-pqVolTime," since last quarter")
-    ,paste0("-",pqVolTime-qVolTime," since last quarter")
-  )
 
 ## Step 8g. Bring together
 pdf(file = paste(directory,"CarkeekWatershedTesting",quarterName,".pdf",sep="")
     ,paper="letter",width=8,height=11)
 
 ggarrange(ggarrange(header,header2,ncol=2,nrow=2,widths=c(2,1))
-        #,tablecolumnnames
-        ,timestested
-        ,testswith2graph
-        ,numvolsgraph
-        ,numHrsGraph
-        #,nrow=6,ncol=1
-        ,nrow=5,ncol=1
+        ,dashboard
+        ,nrow=2,ncol=1
 )
 dev.off()
 
